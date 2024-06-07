13ca38cacba97cb6d47f6d7dd8ae314d44250b09
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2024-06-07 12:52:12 -0400

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
   }
 
   global.db.data.users[m.sender].limit += amount
-  await m.reply(`Genial! minaste *${amount} 🍬 Dulces*`)
+  await m.reply(`Genial! minaste *${amount} 🪙 Coins*`)
   cooldowns[m.sender] = Date.now()
 }
 handler.help = ['minar']
