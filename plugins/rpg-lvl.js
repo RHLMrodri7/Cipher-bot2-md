5f806cb76b4d09039bbce61853d56a9b26679b1f
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2024-06-26 23:33:11 -0400

    Update rpg-lvl.js

--- plugins/rpg-lvl.js
+++ plugins/rpg-lvl.js
@@ -1,40 +1,31 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
-import { levelup } from '../lib/canvas.js'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
+    let img = await (await fetch(`https://telegra.ph/file/b97148e2154508f63d909.jpg`)).buffer()
 	let name = conn.getName(m.sender)
     let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
-        let lvl =  `
-╭────═[ *L E V E L  -  U P* ]═─────⋆
-│╭───────────────···
-┴│✯ *🍭 Nombre* : ${name}
-✩│✯ *📈 Nivel Actual* : ${user.level}
-┬│✯ *💫 XP* : ${user.exp - min}/${xp}
-│╰────────────────···
-╰───────────═┅═──────────
-
-Te falta *${max - user.exp}* de *XP* para subir de nivel
-`.trim()
-await conn.reply(m.chat, lvl, m, adReply)
+        let txt = ` –  *L E V E L U P  -  U S E R*\n\n`
+            txt += `┌  ✩  *Nombre* : ${name}\n`
+            txt += `│  ✩  *Nivel* : ${user.level}\n`
+            txt += `└  ✩  *XP* : ${user.exp - min}/${xp}\n\n`
+            txt += `Te falta *${max - user.exp}* de *💫 XP* para subir de nivel`
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
     }
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-        let str = `
-╭────═[ *L E V E L  -  U P* ]═─────⋆
-│╭───────────────···
-┴│✯ *🍭 Nombre* : ${name}
-✩│✯ *📈 Nivel Actual* : ${user.level}
-┬│✯ *📈 Nivel Anterior* : ${before}
-│╰────────────────···
-╰───────────═┅═──────────
-`.trim()
-await conn.reply(m.chat, str, m, adReply)
-    }
-}
+       let txt = ` –  *L E V E L U P  -  U S E R*\n\n`
+           txt += `┌  ✩  *Nombre* : ${conn.getName(m.sender)}\n`
+           txt += `│  ✩  *Nivel Anterior* : ${before}\n`
+           txt += `└  ✩  *Nivel Actual* : ${user.level}\n\n`
+           txt += `🚩 Cuanto más interactúes con *Ai Hoshino*, mayor será tu Nivel`
 
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+        }
+    }
 handler.help = ['levelup']
 handler.tags = ['rpg']
 
